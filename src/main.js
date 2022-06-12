c90223432170447071702aa5515a5bf648d0322c
Samuel Gunawan <samgun9d32@gmail.com>
2022-06-12 18:51:00 +0700

    Position and camera speed changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
+var cam = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100);
+var renderer = new THREE.WebGL1Renderer({
+    antialias: true
+});
 var scene = new THREE.Scene();
 
-var cam = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100);
-var renderer = new THREE.WebGL1Renderer({antialias: true});
-
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.BasicShadowMap;
+document.body.appendChild(renderer.domElement);
+
 cam.position.z = 5;
 
 // var boxGeo = new THREE.BoxGeometry(1,1,1);
@@ -23,23 +25,25 @@ cam.position.z = 5;
 //     1.0, 1.0, 0.0,
 //     -1.0, 1.0, 0.0,
 // ]);
-// geo_saya.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+// geo_saya.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
 // const mat_saya = new THREE.MeshBasicMaterial({color:0xff0000});
 // let mesh_saya = new THREE.Mesh(geo_saya, mat_saya);
 // scene.add(mesh_saya);
 
 var plane = new THREE.PlaneGeometry(1000, 1000, 500, 500);
-var planeMaterial = new THREE.MeshLambertMaterial({color: 0xaaffaa,});
+var planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xaaffaa
+});
 var planeMesh = new THREE.Mesh(plane, planeMaterial);
 planeMesh.receiveShadow = true;
 planeMesh.position.set(0, -1, 0);
-planeMesh.rotation.x = -Math.PI/2;
+planeMesh.rotation.x = -Math.PI / 2;
 scene.add(planeMesh);
 
-let movement = new THREE.PointerLockControls(cam, renderer.domElement);
 let clock = new THREE.Clock();
+let movement = new THREE.PointerLockControls(cam, renderer.domElement);
 let controls = new THREE.FirstPersonControls(cam, renderer.domElement);
-controls.lookSpeed = 0.1;
+controls.lookSpeed = 0.15;
 // let controls = new THREE.TrwackballControls(cam, renderer.domElement);
 
 var ambient = new THREE.AmbientLight(0x404040);
@@ -57,20 +61,21 @@ scene.add(new THREE.PointLightHelper(pointLight, 0.2, 0x00ff00));
 // scene.add(directionalLight);
 // scene.add(new THREE.DirectionalLightHelper(directionalLight));
 
-var spotlight = new THREE.SpotLight(0x0000ff, 0.5, 5, Math.PI/10);
+var spotlight = new THREE.SpotLight(0x0000ff, 0.5, 5, Math.PI / 10);
 spotlight.position.set(2, 2, 0);
 spotlight.castShadow = true;
 scene.add(spotlight);
 scene.add(new THREE.SpotLightHelper(spotlight));
 
 let building;
-let loader = new THREE.GLTFLoader().load('model/forteresse_royale_de_najac_12/scene.gltf', function(result){
+let loader = new THREE.GLTFLoader().load("model/forteresse_royale_de_najac_12/scene.gltf", (result) => {
     building = result.scene.children[0];
     scene.add(building);
+    building.position.set(0, 0.8, 0);
 });
 
 // adding resizing event listener
-window.addEventListener('resize', function(){
+window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     cam.aspect = window.innerWidth / window.innerHeight;
     cam.updateProjectionMatrix();
@@ -78,48 +83,41 @@ window.addEventListener('resize', function(){
 
 let keyboard = [];
 
-document.body.onkeydown = function(evt){
-    // if(evt.key == 'a'){
+document.body.onkeydown = (evt) => {
+    // if(evt.key == "a"){
     //     cam.position.x += 0.03;
     // }
-    // else if(evt.key == 'd'){
+    // else if(evt.key == "d"){
     //     cam.position.x -= 0.03;
     // }
-    // else if(evt.key == 'w'){
+    // else if(evt.key == "w"){
     //     cam.position.y -= 0.03;
     // }
-    // else if(evt.key == 's'){
+    // else if(evt.key == "s"){
     //     cam.position.y += 0.03;
     // }
     keyboard[evt.key] = true;
 }
 
-document.body.onkeyup = function(evt){
+document.body.onkeyup = (evt) => {
     keyboard[evt.key] = false;
 }
 
-function process_keyboard(){
-    if(keyboard['a']){
+function process_keyboard() {
+    if(keyboard["a"]) {
         movement.moveRight(-0.005);
-    }
-    else if(keyboard['d']){
+    } else if(keyboard["d"]) {
         movement.moveRight(0.005);
-    }
-    else if(keyboard['w']){
+    } else if(keyboard["w"]) {
         movement.moveForward(0.005);
-    }
-    else if(keyboard['s']){
+    } else if(keyboard["s"]) {
         movement.moveForward(-0.005);
     }
 }
 
-function draw(){
-    
-
+function draw() {
     controls.update(clock.getDelta());
-
     process_keyboard();
-
     requestAnimationFrame(draw);
     renderer.render(scene, cam);
 }
