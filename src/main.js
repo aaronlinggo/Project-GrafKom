c3468a3a3442f08808360b85ce1dedb581f08879
aaronlinggo <aaronlinggosatria@gmail.com>
2022-06-12 20:57:12 +0700

    Add button for lightning and camera toggle

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
+// import * as THREE from '../node_modules/three/build/three.module.js';
+// import { TextGeometry } from '../node_modules/three/examples/jsm/geometries/TextGeometry.js';
+
 const color_lantern = 0xf04e03;
 const color_sun = 0x994a0e;
 const color_fog = 0xf59042;
 const speed = 200;
+const pointer = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
 
 let offsetY = 2.5;
-
 let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
 let canJump = false;
+let camControls = false;
+let posXMouse = (window.innerWidth/2);
+let posYMouse = (window.innerHeight/2);
 
 let prevTime = performance.now();
 let time;
@@ -17,6 +24,8 @@ let delta;
 let velocity = new THREE.Vector3();
 let direction = new THREE.Vector3();
 let startWebGL = false;
+
+let lamps = [true, true, true, true];
 
 let overlay = document.createElement("div");
 overlay.setAttribute("id", "pointerLockOverlay");
@@ -27,7 +36,7 @@ overlay.style.width = "100vw";
 overlay.style.height = "100vh";
 overlay.style.zIndex = "1";
 overlay.style.background = "#000";
-overlay.style.opacity = "0.5";
+overlay.style.opacity = "0.7";
 overlay.style.color = "#fff";
 overlay.style.textAlign = "center";
 overlay.style.verticalAlign = "middle";
@@ -35,6 +44,25 @@ overlay.style.lineHeight = "100vh";
 overlay.textContent = "Click to enable controls!";
 overlay.style.display = "block";
 document.body.appendChild(overlay);
+
+let detail = document.createElement("div");
+detail.setAttribute("id", "detail");
+detail.style.position = "absolute";
+detail.style.top = "0px";
+detail.style.left = (window.innerWidth - 250) + "px";
+detail.style.width = "20vw";
+detail.style.height = "13vh";
+detail.style.zIndex = "1";
+detail.style.background = "#000";
+detail.style.opacity = "0.8";
+detail.style.color = "#fff";
+detail.style.textAlign = "center";
+detail.style.verticalAlign = "middle";
+detail.style.display = "table-cell";
+detail.style.whiteSpace = "pre";
+detail.textContent = "Press 'Esc' for exit\r\nPress 'W' for forward\r\nPress 'S' for backward\r\nPress 'A' for left\r\nPress 'D' for right";
+detail.style.display = "block";
+document.body.appendChild(detail);
 
 let dot = document.createElement("div");
 dot.setAttribute("id", "dot");
@@ -44,7 +72,7 @@ dot.style.left = (window.innerWidth / 2) + "px";
 dot.style.width = "5px";
 dot.style.height = "5px";
 dot.style.zIndex = "0";
-dot.style.background = "#ffffff";
+dot.style.background = "green";
 dot.style.display = "none";
 document.body.appendChild(dot);
 
@@ -60,6 +88,7 @@ cam.position.y = 4;
 cam.lookAt(0, 5, 5);
 
 var renderer = new THREE.WebGL1Renderer({
+    powerPreference: "high-performance", // "high-performance", "low-power" or "default". 
     antialias: false, // Should be true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -137,23 +166,6 @@ scene.add(planeMesh);
 var ambient = new THREE.AmbientLight(0x404040);
 scene.add(ambient);
 
-var addPointLight = function (scene, color, x, y, z) {
-    var pointLight = new THREE.PointLight(color);
-    pointLight.position.set(x, y, z);
-    pointLight.add(
-        new THREE.Mesh(
-            new THREE.SphereGeometry(0.1, 10, 10),
-            new THREE.MeshBasicMaterial({
-                color: color,
-            })
-        )
-    );
-    pointLight.castShadow = true;
-    pointLight.shadow.radius = 25;
-    scene.add(pointLight);
-    scene.add(new THREE.PointLightHelper(pointLight, 0.1, 0xff00ff));
-};
-
 var addDirectionalLight = function (scene, color, x, y, z) {
     var directionalLight = new THREE.DirectionalLight(color);
     directionalLight.position.set(x, y, z);
@@ -164,10 +176,66 @@ var addDirectionalLight = function (scene, color, x, y, z) {
     scene.add(new THREE.DirectionalLightHelper(directionalLight));
 };
 
-addPointLight(scene, color_lantern, -17.5, 2.5, -28.5);
-addPointLight(scene, color_lantern, -17.5, 2.5, 24.5);
-addPointLight(scene, color_lantern, 17.5, 2.5, 24.5);
-addPointLight(scene, color_lantern, 17.5, 2.5, -28.5);
+var pointLight1 = new THREE.PointLight(color_lantern);
+pointLight1.position.set(-17.5, 2.5, -28.5);
+pointLight1.add(
+    new THREE.Mesh(
+        new THREE.SphereGeometry(0.1, 10, 10),
+        new THREE.MeshBasicMaterial({
+            color: color_lantern,
+        })
+    )
+);
+pointLight1.castShadow = true;
+pointLight1.shadow.radius = 25;
+scene.add(pointLight1);
+scene.add(new THREE.PointLightHelper(pointLight1, 0.1, 0xff00ff));
+
+var pointLight2 = new THREE.PointLight(color_lantern);
+pointLight2.position.set(-17.5, 2.5, 24.5);
+pointLight2.add(
+    new THREE.Mesh(
+        new THREE.SphereGeometry(0.1, 10, 10),
+        new THREE.MeshBasicMaterial({
+            color: color_lantern,
+        })
+    )
+);
+pointLight2.castShadow = true;
+pointLight2.shadow.radius = 25;
+scene.add(pointLight2);
+scene.add(new THREE.PointLightHelper(pointLight2, 0.1, 0xff00ff));
+
+var pointLight3 = new THREE.PointLight(color_lantern);
+pointLight3.position.set(17.5, 2.5, 24.5);
+pointLight3.add(
+    new THREE.Mesh(
+        new THREE.SphereGeometry(0.1, 10, 10),
+        new THREE.MeshBasicMaterial({
+            color: color_lantern,
+        })
+    )
+);
+pointLight3.castShadow = true;
+pointLight3.shadow.radius = 25;
+scene.add(pointLight3);
+scene.add(new THREE.PointLightHelper(pointLight3, 0.1, 0xff00ff));
+
+var pointLight4 = new THREE.PointLight(color_lantern);
+pointLight4.position.set(17.5, 2.5, -28.5);
+pointLight4.add(
+    new THREE.Mesh(
+        new THREE.SphereGeometry(0.1, 10, 10),
+        new THREE.MeshBasicMaterial({
+            color: color_lantern,
+        })
+    )
+);
+pointLight4.castShadow = true;
+pointLight4.shadow.radius = 25;
+scene.add(pointLight4);
+scene.add(new THREE.PointLightHelper(pointLight4, 0.1, 0xff00ff));
+
 addDirectionalLight(scene, color_sun, 0, 40, 50);
 
 // adding resizing event listener
@@ -176,6 +244,11 @@ window.addEventListener("resize", () => {
     cam.aspect = window.innerWidth / window.innerHeight;
     cam.updateProjectionMatrix();
 });
+
+// Orbit Controls
+const orbitControls = new THREE.OrbitControls( cam, renderer.domElement );
+orbitControls.update();
+orbitControls.enabled = false;
 
 // First Person Controls
 let fpsControls = new THREE.PointerLockControls(cam, renderer.domElement);
@@ -204,10 +277,13 @@ fpsControls.addEventListener(
 fpsControls.addEventListener(
     "unlock",
     () => {
-        overlay.style.display = "block";
-        dot.style.display = "none";
-        startWebGL = false;
-        velocity = new THREE.Vector3();
+        if (!camControls){
+            overlay.style.display = "block";
+            dot.style.display = "none";
+            startWebGL = false;
+            velocity = new THREE.Vector3();
+
+        }
     },
     false
 );
@@ -232,6 +308,14 @@ const onKeyDown = function (event) {
         case 39: // right
         case 68: // d
             moveRight = true;
+            break;
+        
+        case 27: // esc
+            if (fpsControls.isLocked) {
+                fpsControls.unlock();
+                overlay.style.display = "block";
+                dot.style.display = "none";
+            }
             break;
     }
 };
@@ -428,7 +512,7 @@ function animate() {
             prevTime = time;
         }
     }
-
+    // orbitControls.update();
     requestAnimationFrame(animate);
     drawStoneLantern();
 
@@ -471,7 +555,7 @@ let loader4 = new THREE.GLTFLoader();
 const wall1 = new THREE.Object3D();
 const wall2 = new THREE.Object3D();
 const wall3 = new THREE.Object3D();
-loader4.load("./models/modular_concrete_fence/scene3.gltf", processWall);
+loader4.load("./dump/models/modular_concrete_fence/scene.gltf", processWall);
 
 function processWall(gltf) {
     const box = new THREE.Box3().setFromObject(gltf.scene);
@@ -550,3 +634,145 @@ function dumpObject(obj, lines = [], isLast = true, prefix = "") {
     });
     return lines;
 }
+
+const geometry = new THREE.BoxGeometry( 0.5, 3, 3 );
+let loadermaterial = new THREE.TextureLoader();
+const material = new THREE.MeshBasicMaterial( { map: loadermaterial.load("../textures/black-g444986ca3_1920.jpg") } );
+material.castShadow = true;
+material.receiveShadow = true;
+const cube = new THREE.Mesh( geometry, material );
+cube.position.set(-23, 3, 30);
+cube.rotation.y = Math.PI / 2;
+cube.castShadow = true;
+cube.receiveShadow = true;
+scene.add( cube );
+
+const btn1 = new THREE.BoxGeometry( 0.1, 0.2, 0.2 );
+const btnMaterial1 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const btn1cube = new THREE.Mesh( btn1, btnMaterial1 );
+btn1cube.position.set(-24, 3.5, 30.3);
+btn1cube.rotation.y = Math.PI / 2;
+btn1cube.name = "btn1";
+scene.add( btn1cube );
+
+const btn2 = new THREE.BoxGeometry( 0.1, 0.2, 0.2 );
+const btnMaterial2 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const btn2cube = new THREE.Mesh( btn2, btnMaterial2 );
+btn2cube.position.set(-22, 3.5, 30.3);
+btn2cube.rotation.y = Math.PI / 2;
+btn2cube.name = "btn2";
+scene.add( btn2cube );
+
+const btn3 = new THREE.BoxGeometry( 0.1, 0.2, 0.2 );
+const btnMaterial3 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const btn3cube = new THREE.Mesh( btn3, btnMaterial3 );
+btn3cube.position.set(-24, 3, 30.3);
+btn3cube.rotation.y = Math.PI / 2;
+btn3cube.name = "btn3";
+scene.add( btn3cube );
+
+const btn4 = new THREE.BoxGeometry( 0.1, 0.2, 0.2 );
+const btnMaterial4 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const btn4cube = new THREE.Mesh( btn4, btnMaterial4 );
+btn4cube.position.set(-22, 3, 30.3);
+btn4cube.rotation.y = Math.PI / 2;
+btn4cube.name = "btn4";
+scene.add( btn4cube );
+
+const btn5 = new THREE.BoxGeometry( 0.1, 0.2, 0.2 );
+const btnMaterial5 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const btn5cube = new THREE.Mesh( btn5, btnMaterial5 );
+btn5cube.position.set(-22, 2, 30.3);
+btn5cube.rotation.y = Math.PI / 2;
+btn5cube.name = "btn5";
+scene.add( btn5cube );
+
+var textGeo = new THREE.TextGeometry( "My Text", {
+    font: "helvetiker",
+    size: 0.2,
+    height: 0.1,
+    curveSegments: 21,
+    bevelEnabled: false
+} );
+
+var textMaterial = new THREE.MeshPhongMaterial( { color: 0xdddddd } );
+var mesh = new THREE.Mesh( textGeo, textMaterial );
+mesh.position.set(-23, 3, 32);
+scene.add( mesh );
+
+const onMouseClick = (event) => {
+    if (camControls == false){
+        pointer.x = ((window.innerWidth/2) / window.innerWidth) * 2 - 1;
+        pointer.y = -((window.innerHeight/2) / window.innerHeight) * 2 + 1;
+    }
+    else{
+        pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+        pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    }
+    raycaster.setFromCamera(pointer, cam);
+    const intersects = raycaster.intersectObjects(scene.children);
+    if (intersects.length > 0) {
+        if (intersects[0].object.name === "btn1") {
+            if (lamps[0] == true){
+                lamps[0] = false;
+                pointLight1.visible = false;
+                intersects[0].object.material.color.set(0xff0000);
+            }
+            else{
+                lamps[0] = true;
+                pointLight1.visible = true;
+                intersects[0].object.material.color.set(0xffffff);
+            }
+        } else if (intersects[0].object.name === "btn2") {
+            if (lamps[3] == true){
+                lamps[3] = false;
+                pointLight4.visible = false;
+                intersects[0].object.material.color.set(0xff0000);
+            }
+            else{
+                lamps[3] = true;
+                pointLight4.visible = true;
+                intersects[0].object.material.color.set(0xffffff);
+            }
+        }else if (intersects[0].object.name === "btn3") {
+            if (lamps[1] == true){
+                lamps[1] = false;
+                pointLight2.visible = false;
+                intersects[0].object.material.color.set(0xff0000);
+            }
+            else{
+                lamps[1] = true;
+                pointLight2.visible = true;
+                intersects[0].object.material.color.set(0xffffff);
+            }
+        }else if (intersects[0].object.name === "btn4") {
+            if (lamps[2] == true){
+                lamps[2] = false;
+                pointLight3.visible = false;
+                intersects[0].object.material.color.set(0xff0000);
+            }
+            else{
+                lamps[2] = true;
+                pointLight3.visible = true;
+                intersects[0].object.material.color.set(0xffffff);
+            }
+        }else if (intersects[0].object.name === "btn5") {
+            if (camControls == false){
+                camControls = true;
+                intersects[0].object.material.color.set(0xffffff);
+                fpsControls.unlock();
+                document.getElementById("dot").style.display = "none";
+                orbitControls.enabled = true;
+            }
+            else{
+                camControls = false;
+                intersects[0].object.material.color.set(0x00ff00);
+                orbitControls.enabled = false;
+                fpsControls.lock();
+                document.getElementById("dot").style.display = "true";
+            }
+        }
+    }
+}
+
+window.addEventListener('click', onMouseClick);
